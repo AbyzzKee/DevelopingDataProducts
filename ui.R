d406853c376da3a020e7193eedaf0d436a7aee5d
AbyzzKee <abyzzkee@gmail.com>
2017-04-30 17:28:04 +0800

    add documentation

--- ui.R
+++ ui.R
@@ -3,12 +3,17 @@ library(shiny)
 shinyUI(pageWithSidebar(
   headerPanel("Seasonal Bread Sales Viewer"),
   sidebarPanel(
-    sliderInput('daySlider', 'Date Slider',value = 5, min = 1, max = 10, step = 0.14),
+    sliderInput('daySlider', 'Date Slider (Slide to select date)',value = 5, min = 1, max = 10, step = 0.14),
     h4("Date:"),
-    textOutput('text1')
+    textOutput('text1'),
+    br(),
+    h4("Documentation:"),
+    a("Click to show Documentation Slide", 
+      href="https://AbyzzKee.github.io/DevelopingDataProducts/Lesson9_W4_Assignment.html")
   ),
   mainPanel(
     h4("Bread Product Tab"),
+    h5("Click on tab to select different product"),
     tabsetPanel(type = "tabs",
                 tabPanel("GARDENIA CLASSIC", plotOutput('plot1')),
                 tabPanel("GARDENIA BONANZA", plotOutput('plot2')),
